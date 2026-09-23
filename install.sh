8ff00d98a395c6c971a6133de06ad612c5fa8dac
PhilippKosarev <philipp.kosarev@gmail.com>
2026-09-23 17:12:36 +0100

    remove unnecessary install.sh script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Uninstalling..." &&
-pip uninstall acd -y > /dev/null &&
-echo "Building..." &&
-rm ./dist -rf &&
-python3 -m build > /dev/null &&
-echo "Installing..." &&
-pip install dist/acd*.whl > /dev/null
